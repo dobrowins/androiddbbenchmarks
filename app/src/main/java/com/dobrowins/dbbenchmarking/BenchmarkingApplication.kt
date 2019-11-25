cf0f1b6427b5990e9a04bf4d30a1ab4f9b3f47d6
Artem <dobrowins@gmail.com>
2019-11-25 22:46:06 +0300

    readme правки

---
+++ app/src/main/java/com/dobrowins/dbbenchmarking/BenchmarkingApplication.kt
@@ -0,0 +1,11 @@
+package com.dobrowins.dbbenchmarking
+
+import android.app.Application
+import com.orhanobut.hawk.Hawk
+import io.paperdb.Paper
+import io.realm.Realm
+
+
+class BenchmarkingApplication : Application() {
+
+}
